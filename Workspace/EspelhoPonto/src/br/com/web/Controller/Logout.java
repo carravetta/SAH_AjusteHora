e5ef937d677b1eb0c5a6317e48ddc6355d4fbf75
FernandoBuonocore <fernando_beckenbach@hotmail.com>
2018-03-28 22:11:11 -0300

    ajustes
    Ajuste no projeto para redirecionar e logout

--- Workspace/EspelhoPonto/src/br/com/web/Controller/Logout.java
+++ Workspace/EspelhoPonto/src/br/com/web/Controller/Logout.java
@@ -2,6 +2,7 @@ package br.com.web.Controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
@@ -16,12 +17,23 @@ public class Logout extends HttpServlet{
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		Cookie[] cookies = req.getCookies();
 	
+		String usuario = "Nenhum";
+		
+		//Assim tira o cookie do navegador e desloga o usuario
 		for(Cookie cookie : cookies){
 			if(cookie.getName().equals("usuario.logado")){
+				usuario =  cookie.getValue();
 				cookie.setMaxAge(0);
 				resp.addCookie(cookie);
 			}
 		}
+		
+		//para o atributo para o jsp, assim mostra na tela o que passar nesse parametro
+		req.setAttribute("usuario", usuario);
+		
+		//Assim redireciona para outra pagina html
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/paginas/logout.jsp");
+		dispatcher.forward(req, resp);
 	}
 	
 }
